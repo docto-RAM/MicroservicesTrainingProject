ddb2d2e39ab335bb4190f01339f394e1d42c02c5
a.rohlikov <a.rohlikov@itransition.com>
2024-01-30 22:58:56 +0300

    Add Custom Properties to User Table

--- Mango/Mango.Services.AuthAPI/Program.cs
+++ Mango/Mango.Services.AuthAPI/Program.cs
@@ -1,4 +1,5 @@
 using Mango.Services.AuthAPI.Data;
+using Mango.Services.AuthAPI.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
@@ -10,7 +11,7 @@ builder.Services.AddDbContext<AppDbContext>(option =>
     option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 builder.Services
-    .AddIdentity<IdentityUser, IdentityRole>()
+    .AddIdentity<ApplicationUser, IdentityRole>()
     .AddEntityFrameworkStores<AppDbContext>()
     .AddDefaultTokenProviders();
 
