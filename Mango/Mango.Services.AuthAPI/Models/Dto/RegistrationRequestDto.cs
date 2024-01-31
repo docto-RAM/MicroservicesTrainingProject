688193e14af4ea3cc37690ba45a89ea9d8b1a259
a.rohlikov <a.rohlikov@itransition.com>
2024-01-31 22:34:33 +0300

    Register Endpoint in Auth Service

--- Mango/Mango.Services.AuthAPI/Models/Dto/RegistrationRequestDto.cs
+++ Mango/Mango.Services.AuthAPI/Models/Dto/RegistrationRequestDto.cs
@@ -2,9 +2,9 @@
 {
     public class RegistrationRequestDto
     {
-        public string Id { get; set; }
         public string Email { get; set; }
         public string Name { get; set; }
+        public string PhoneNumber { get; set; }
         public string Password { get; set; }
     }
 }
