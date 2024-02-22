c5c27fcd63822ac83232740298c664a80eede99f
a.rohlikov <a.rohlikov@itransition.com>
2024-02-22 23:19:04 +0300

    Setup Email and DTO's

---
+++ Mango/Mango.Services.EmailAPI/Controllers/EmailAPIController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Services.EmailAPI.Controllers
+{
+    [Route("api/email")]
+    [ApiController]
+    public class EmailAPIController : ControllerBase
+    {
+    }
+}
