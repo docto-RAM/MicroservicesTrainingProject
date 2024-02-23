4274c0b01fb9eb0f7215cdc100da5afe41164b4b
a.rohlikov <a.rohlikov@itransition.com>
2024-02-23 16:05:04 +0300

    Register Methods to Processor

--- Mango/Mango.Services.EmailAPI/Messaging/AzureServiceBusConsumer.cs
+++ Mango/Mango.Services.EmailAPI/Messaging/AzureServiceBusConsumer.cs
@@ -1,8 +1,11 @@
 ﻿using Azure.Messaging.ServiceBus;
+using Mango.Services.EmailAPI.Models.Dto;
+using Newtonsoft.Json;
+using System.Text;
 
 namespace Mango.Services.EmailAPI.Messaging
 {
-    public class AzureServiceBusConsumer
+    public class AzureServiceBusConsumer : IAzureServiceBusConsumer
     {
         private readonly string serviceBusConnectionString;
         private readonly string emailShoppingCartQueue;
@@ -20,6 +23,42 @@ namespace Mango.Services.EmailAPI.Messaging
 
             var client = new ServiceBusClient(serviceBusConnectionString);
             _emailCartProcessor = client.CreateProcessor(emailShoppingCartQueue);
+        }
+
+        public async Task Start()
+        {
+            _emailCartProcessor.ProcessMessageAsync += OnEmailCartRequestReceived;
+            _emailCartProcessor.ProcessErrorAsync += ErrorHandler;
+        }
+
+        public async Task Stop()
+        {
+            await _emailCartProcessor.StopProcessingAsync();
+            await _emailCartProcessor.DisposeAsync();
+        }
+
+        private Task ErrorHandler(ProcessErrorEventArgs args)
+        {
+            Console.WriteLine(args.Exception.ToString());
+            return Task.CompletedTask;
+        }
+
+        private async Task OnEmailCartRequestReceived(ProcessMessageEventArgs args)
+        {
+            var message = args.Message;
+            var body = Encoding.UTF8.GetString(message.Body);
+            CartDto objMessage = JsonConvert.DeserializeObject<CartDto>(body);
+
+            try
+            {
+                // TODO: Log email.
+
+                await args.CompleteMessageAsync(args.Message);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
         }
     }
 }
