451ed975d9da0dea923da060a72c2b32559125cf
a.rohlikov <a.rohlikov@itransition.com>
2024-03-04 17:41:09 +0300

    Order List UI

---
+++ Mango/Mango.Web/Controllers/OrderController.cs
@@ -0,0 +1,20 @@
+﻿using Mango.Web.Service.IService;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Web.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly IOrderService _orderService;
+
+        public OrderController(IOrderService orderService)
+        {
+            _orderService = orderService;
+        }
+
+        public IActionResult OrderIndex()
+        {
+            return View();
+        }
+    }
+}
