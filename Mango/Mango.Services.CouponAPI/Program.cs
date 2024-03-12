a001c99c90fa835e4feb50c21d0e31933c401cb2
a.rohlikov <a.rohlikov@itransition.com>
2024-03-12 19:46:22 +0300

    Configure Swagger UI

--- Mango/Mango.Services.CouponAPI/Program.cs
+++ Mango/Mango.Services.CouponAPI/Program.cs
@@ -55,11 +55,15 @@ builder.Services.AddAuthorization();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    if (!app.Environment.IsDevelopment())
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", nameof(Mango.Services.CouponAPI));
+        c.RoutePrefix = string.Empty;
+    }
+});
 
 Stripe.StripeConfiguration.ApiKey = builder.Configuration.GetSection("Stripe:SecretKey").Get<string>();
 
--- Mango/Mango.Services.ShoppingCartAPI/Program.cs
+++ Mango/Mango.Services.ShoppingCartAPI/Program.cs
@@ -75,11 +75,15 @@ builder.Services.AddAuthorization();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    if (!app.Environment.IsDevelopment())
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", nameof(Mango.Services.ShoppingCartAPI));
+        c.RoutePrefix = string.Empty;
+    }
+});
 
 app.UseHttpsRedirection();
 app.UseAuthentication();
